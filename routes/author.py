02dd15b5350ac3ee26a8deb8d3e2d3db359fad71
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-25 22:16:10 +0200

    author blueprint is added

--- routes/author.py
+++ routes/author.py
@@ -11,11 +11,29 @@ def getallAuthor():
         "ok":True, "data": data,
     }))
 
+@router.route("/<int:authId>", methods=["GET"])
+def getParticularAuhor(authId):
+    author = model.Author.query.filter_by(id=authId)
+    author = model.AuthorSchema().dump(author)
+    return flask.make_response(flask.jsonify({
+        "ok": True,
+        "data": autor,
+    }))
+
+
 @router.route("/", methods=["POST"])
 def addAuthor():
     author = flask.request.get_json()
     model.author.Author(**author).create()
     return flask.make_response(flask.jsonify({
         "ok": True,
+    }))
+
+@router.router("/<int:authId>", methods=["DELETE"])
+def deleteAuthor(authId):
+    AutherObj = model.Author.query.filter_by(id=authId)
+    AutherObj.delete()
+    return flask.make_response(flask.jsonify({
+        "ok":True,
     }))
 
